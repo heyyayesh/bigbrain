1e9c1ab150e8df5fed72826d13f35f6fcb631eab
heyyayesh <biswalayesh3@gmail.com>
2022-03-22 21:01:40 +0530

    change name

---
+++ README.md
@@ -0,0 +1,5 @@
+# Big Brain
+
+This is a trivia app created using React that fetches data from [Open Trivia Database](https://opentdb.com/api_config.php).
+
+Live preview: [Big Brain](https://heyyayesh.github.io/bigbrain)
--- src/components/startpage/StartPage.js
+++ src/components/startpage/StartPage.js
@@ -6,8 +6,8 @@ import blobs from '../../imgs/bottomBlob.png';
 function StartPage(props) {
   return (
     <div className={styles.startPage}>
-      <h1 className={styles.title}>Quizzical</h1>
-      <p className={styles.desc}>Test your general knowledge!</p>
+      <h1 className={styles.title}>Big Brain</h1>
+      <p className={styles.desc}>Answer questions and feel your brain grow!</p>
       <button className={styles.startBtn} onClick={props.handleClick}>Start Quiz</button>
       <img className={styles.topBlob} src={blob5} alt='blob'/>
       <img className={styles.bottomBlob} src={blobs} alt='blob'/>
